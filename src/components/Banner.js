9652442527c77e366d54bd5ed875b3202ecf4831
jojotier <122423763+jojotier@users.noreply.github.com>
2023-07-19 13:00:51 -0500

    Figured out how to add a banner without breaking the app!

---
+++ src/components/Banner.js
@@ -0,0 +1,10 @@
+const Banner = () => {
+    return(
+        <div>
+            <header>Orbit Report</header>
+            <p>Click on the buttons below to learn more about satelites and their orbit types!</p>
+        </div>
+    )
+}
+
+export default Banner;
