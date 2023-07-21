4abf7f70775ffb4f224e53fa747249306de8e67d
jojotier <122423763+jojotier@users.noreply.github.com>
2023-07-21 17:48:46 -0500

    ran npm test and passed!

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -2,7 +2,7 @@ const Banner = () => {
     return(
         <div>
             <header>Orbit Report</header>
-            <p>Click on the buttons below to learn more about satelites and their orbit types!</p>
+           <p>Click on the buttons below to learn more about satelites and their orbit types!</p>
         </div>
     )
 }
